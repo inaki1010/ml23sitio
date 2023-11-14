85f9e574b342387a0d453b150fd250328e259691
inaki1010 <inakiperez003@gmail.com>
2023-11-13 19:43:52 -0800

    proy222

--- app/page.jsx
+++ app/page.jsx
@@ -80,7 +80,7 @@ export default function Home() {
     <Card className="col-span-12 sm:col-span-4 h-[300px]">
       <CardHeader className="absolute z-10 top-1 flex-col !items-start">
         <p className="text-tiny text-white/60 uppercase font-bold">Proyecto final</p>
-        <h4 className="text-white font-medium text-large">????</h4>
+        <h4 className="text-white font-medium text-large">???</h4>
       </CardHeader>
       <Image
         removeWrapper
